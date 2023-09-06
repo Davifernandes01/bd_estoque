70dc03f031d220ac139fa1b0b95d8080e6b80988
rada <rada@gmail.com>
2023-09-06 09:36:59 -0300

    criação da classe produtos

--- Program.cs
+++ Program.cs
@@ -1,13 +1,27 @@
-﻿namespace estoque
-{
-    
+﻿namespace Product;
+
 
     internal class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           var p1 = new Product(1,"tv", "tv 23polegada", 4,DateTime.Now);
+
+            Console.WriteLine(p1.id);
+            Console.WriteLine(p1.name);
+            Console.WriteLine(p1.description);
+            Console.WriteLine(p1.last_Delivery_date);
+
+             Console.WriteLine("---------------");
+
+            Console.WriteLine(p1.UpdateDescription("atualizando a descrição")); 
+            Console.WriteLine(p1.isIncreaseAmount(3));
+            Console.WriteLine(p1.isDecreaseAmount(2));
+            Console.WriteLine(p1.UpdateName("teste"));
+
+            Console.WriteLine($"data de saida {p1.get_last_Departure_date(new DateTime(2023,07,02,09,30,09))},");
+
+            Console.WriteLine(p1);
         }
     }
 
-}
---
+++ model/Product.cs
@@ -0,0 +1,96 @@
+
+using System.Security.Cryptography;
+namespace Product 
+{
+    class Product {
+
+        public int id { get;  private set;}
+        public string name { get ; private set;}
+        public string description { get; private set;}  
+        public int amount { get; private set;} 
+        public  DateTime last_Delivery_date { get; private set;} //data de entrada
+        public DateTime last_Departure_date {get; private set;}// data de saida
+
+        public Product(int id, string name, string description, int amount, 
+        DateTime last_Delivery_date)
+        {
+            this.id = id;
+            this.name = name;
+            this.description = description;
+            this.amount = amount;
+            this.last_Delivery_date = last_Delivery_date;
+          
+            
+        }
+
+
+        public int isIncreaseAmount(int amount){ //metado para aumentar a quantidade do produto
+            if(amount > 0){
+
+                this.amount += amount;
+                
+            }
+
+            return this.amount;
+        }
+
+        public int isDecreaseAmount(int amount){ //metado para diminuir a quantidade de produto
+
+            if(amount > 0){
+
+                this.amount -= amount;
+                
+            }
+
+                return this.amount;    
+        }
+
+        public string UpdateDescription(string update){ // metado para atualizar a descricao
+
+            if (string.IsNullOrWhiteSpace(update)){
+
+               return "";
+                
+            }
+
+                return description = update;
+            
+        }
+
+        public string UpdateName(string name){// metado para  atualizar o nome do produto
+            if(string.IsNullOrWhiteSpace(name)){
+                
+                
+                return "";
+             
+            }
+
+            return this.name = name;
+        }
+
+        public DateTime get_last_Departure_date( DateTime date){//metado para setar a data de saida
+
+            return last_Departure_date = date;
+        }
+
+        //metados toString, hashcode e equals
+
+        public override string ToString()
+        {
+            return string.Format
+            ("Produto [id: {0}], [nome: {1}], [descriçaõ: {2}], [quantidade: {3}], [data de entrada: {4}], [data de saida: {5}] ",
+            id, name,description, amount, last_Delivery_date, last_Departure_date);
+        }
+
+
+        public override bool Equals(object? obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
